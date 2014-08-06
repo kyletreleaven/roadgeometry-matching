bcc752771ac5862348ac180768d17a0ebe347036
Kyle Treleaven <treleaven.kyle@gmail.com>
2014-08-06 13:25:59 -0400

    first cut congestion matching; next step: traversal with path transcription, and interesting test cases

--- setiptah/roadbm/congestion.py
+++ setiptah/roadbm/congestion.py
@@ -15,11 +15,16 @@ import scipy.signal as sig
 
 
 def BIPARTITEMATCH_ROADS_CONGESTED( S, T, roadmap, congestion_dict ) :
+    """
+    as vanilla bipartite matching on roadmaps, except
+    congestion_dict associates with each road an integer function describing the 
+    cost per match, per unit length, associated with any interval supporting n matches 
+    """
     MATCH = []
     
     segment_dict = roadbm.SEGMENTS( S, T, roadmap )
     surplus_dict = dict()
-    objective_dict = dict()
+    measure_dict = dict()
     
     for road, segment in segment_dict.iteritems() :
         match = roadbm.PREMATCH( segment )
@@ -27,22 +32,12 @@ def BIPARTITEMATCH_ROADS_CONGESTED( S, T, roadmap, congestion_dict ) :
         
         surplus_dict[road] = roadbm.SURPLUS( segment )
         
-        roadlen = roadbm.get_road_data( road, roadnet ).get( 'length', 1 )
+        roadlen = roadbm.get_road_data( road, roadmap ).get( 'length', 1 )
         measure = roadbm.MEASURE( segment, roadlen )
+        measure_dict[road] = measure
         
-        objective_dict[road] = CONGESTION_OBJECTIVE( measure, congestion_dict[road] )
-        #objective_dict[road] = objective
-        
-        
-    from nxflow.capscaling import SOLVER
-    try :
-        assist = SOLVER( roadmap, surplus_dict, objective_dict )
-    except Exception as ex :
-        ex.segs = segment_dict
-        ex.surp = surplus_dict
-        ex.obj = objective_dict
-        
-        raise ex
+    N = len(S) - len(MATCH)     # should be a better way...?
+    assist = SOLVER( roadmap, surplus_dict, measure_dict, congestion_dict, N )
     
     if False :        # activate for debug
         imbalance = roadbm.CHECKFLOW( assist, roadmap, surplus_dict )
@@ -54,9 +49,10 @@ def BIPARTITEMATCH_ROADS_CONGESTED( S, T, roadmap, congestion_dict ) :
     except Exception as ex :
         ex.imbal = imbalance
         raise ex
-    
+ 
     topograph = roadbm.TOPOGRAPH( segment_dict, assist, roadmap )
-    
+ 
+    # will need a more informative TRAVERSE method    
     try :
         match = roadbm.TRAVERSE( topograph )
     except Exception as ex :
@@ -70,8 +66,89 @@ def BIPARTITEMATCH_ROADS_CONGESTED( S, T, roadmap, congestion_dict ) :
 
 
 
-def CONGESTION_OBJECTIVE( measure, rho ) :
+
+
+
+
+
+
+def SOLVER( roadnet, surplus, measure_dict, congestion_dict, N ) :
+    from setiptah.basic_graph.mygraph import mygraph
+    from setiptah.nxopt.cvxcostflow import MinConvexCostFlow
+
+    network = mygraph()
+    capacity = {}
+    supply = { i : 0. for i in roadnet.nodes() }
+    cost = {}   # functions
+    #
+    oneway_offset = {}  # for one-way roads
     
+    for i,j, road, data in roadnet.edges_iter( keys=True, data=True ) :
+        supply[j] += surplus[road]
+        measure = measure_dict[road]
+        rho = congestion_dict[road]
+                
+        fobj = CONGESTION_OBJECTIVE( measure, rho, N )
+        
+        # edge construction
+        if data.get( 'oneway', False ) :
+            # if one-way road
+            
+            # record minimum allowable flow on road
+            zmin = -measure.min_key()   # i.e., z + min key of measure >= 0 
+            oneway_offset[road] = zmin
+            # create a 'bias point'
+            supply[i] -= zmin
+            supply[j] += zmin
+            
+            # shift and record the cost function on only a forward edge
+            fobj_offset = roadbm.offsetWrapper( fobj, zmin )
+            network.add_edge( road, i, j )
+            cost[ road ] = fobj_offset
+            
+        else :
+            # if bi-directional road... instantiate pair of edges
+            #cc = roadbm.costWrapper( cost_data )
+            n_fobj = roadbm.negativeWrapper( fobj )     # won't have to worry about the C(0) offset
+            
+            network.add_edge( (road,+1), i, j )
+            cost[ (road,+1) ] = fobj
+            #
+            network.add_edge( (road,-1), j, i )
+            cost[ (road,-1) ] = n_fobj
+
+    """ U <= N ; """
+    f = MinConvexCostFlow( network, {}, supply, cost, N )
+    
+    flow = {}
+    for i, j, road in roadnet.edges_iter( keys=True ) :
+        if road in oneway_offset :
+            flow[road] = f[road] + oneway_offset[road]
+        else :
+            flow[road] = f[(road,+1)] - f[(road,-1)]
+            
+        flow[road] = int( flow[road] )
+    
+    #print flow
+    return flow
+
+
+
+
+
+
+
+
+
+
+def CONGESTION_OBJECTIVE( measure, rho, N, efficient=True ) :
+    obj_data = CONGESTION_OBJECTIVE_DATA( measure, rho, N, efficient )
+    return RBTreeLinterp( obj_data )
+
+
+
+def CONGESTION_OBJECTIVE_DATA( measure, rho, N, efficient=True ) :
+        
     """ prepare convolution """
     a = measure.min_key()
     b = measure.max_key()
@@ -110,16 +187,56 @@ def CONGESTION_OBJECTIVE( measure, rho ) :
             
     else :
         alpha_seq = [ alpha[k] for k in xrange(A,B+1) ]
+        
     
-    # efficient, FFT method
-    C_fft = sig.fftconvolve( W, alpha_seq )
-    C_fix = C_fft[b-A-N:b-A+N+1]
-
-    # return some wrapper for C_fix
+    # perform convolution    
+    res = bintrees.RBTree()     # not just any map!
+    Z = xrange(-N,N+1)
     
+    if not efficient :
+                
+        # long-hand convolution, for verification
+        C_manual = {}
+        for z in Z :
+            C_manual[z] = 0.
+            for k, w in measure.items() :
+                n = k + z
+                C_manual[z] += alpha[n] * w
+    
+        res.update( C_manual )
+        
+    else :
+        # efficient, FFT method
+        C_fft = sig.fftconvolve( W, alpha_seq )
+        C_fix = C_fft[b-A-N:b-A+N+1]
+        
+        res.update( zip( Z, C_fix ) )
+        
+    # actually... wrap this first
+    return res
 
 
 
+
+class RBTreeLinterp :
+    """
+    linear interpolation wrapper:
+    callable returns linear interpolation between floor_key and ceil_key;
+    thus, valid for any real value between minkey and maxkey.  
+    """ 
+    def __init__(self, rbtree ) :
+        assert isinstance(rbtree, bintrees.RBTree )
+        self.tree = rbtree
+        
+    def __call__(self, z ) :
+        z1, f1 = self.tree.floor_item(z)
+        z2, f2 = self.tree.ceiling_item(z)
+        
+        if z2 > z1 :
+            m = float( f2 - f1 ) / ( z2 - z1 )
+            return f1 + m * ( z - z1 )
+        else :
+            return f1
 
 
 
@@ -137,128 +254,45 @@ if __name__== '__main__' :
     #rho = lambda x : np.power( x, 2. )          # square law, why not!
     rho = lambda x : abs(x)                      # linear congestion?
 
-    N = 10
+    NUMPOINTS = 10
 
 
-    # instance    
-    sampler = roadbm.UniformDist(roadmap)
+    # instance
+    import setiptah.roadgeometry.probability as roadprob
+    sampler = roadprob.UniformDist(roadmap)
     
-    S = [ sampler.sample() for i in xrange(N) ]
-    T = [ sampler.sample() for i in xrange(N) ]
+    S = [ sampler.sample() for i in xrange(NUMPOINTS) ]
+    T = [ sampler.sample() for i in xrange(NUMPOINTS) ]
     
     # algorithm
     # stolen form ROADSBIPARTITEMATCH    
     
-    MATCH = []
-    
-    segment_dict = roadbm.SEGMENTS( S, T, roadmap )
-    measure_dict = {}
-    
-    for road, segment in segment_dict.iteritems() :
-        match = roadbm.PREMATCH( segment )
-        MATCH.extend( match )
-        
-        #surplus_dict[road] = SURPLUS( segment )
-        
-        roadlen = ROAD.get_road_data( road, roadmap ).get( 'length', 1 )
-        measure = roadbm.MEASURE( segment, roadlen )
-        measure_dict[road] = measure
-        #objective_dict[road] = OBJECTIVE( measure )
-        #objective_dict[road] = objective
-    """ measure_dict now contains sequence of W_n """
+    rho_dict = { road : rho for i,j,road in roadmap.edges_iter(keys=True) }
+    match = BIPARTITEMATCH_ROADS_CONGESTED( S, T, roadmap, rho_dict ) 
 
-    
-    
-    """ prepare convolution """
-    a = measure.min_key()
-    b = measure.max_key()
-    
-    # serialize
-    if True :
-        W = [ w for k, w in measure.items() ]
-        W.reverse()
+    if False :    
+        segment_dict = roadbm.SEGMENTS( S, T, roadmap )
+        measure_dict = {}
         
-    else :
-        W = np.zeros(b-a + 1 )
-        for k, w in measure.items() :
-            W[-k] = w   # reverse here, and account for the circular shift!
-    
-    """
-        need C(z) for z \in [-N,N] ??? is this true?
-        so, W_{n-z} = 0 for all n < a - N, and for all n > b + N
-    """
-    # relevant range
-    A, B = a - N, b + N
-    L = B-A + 1
-    
-    alpha = {}
-    for n in xrange(A,B+1) :
-        alpha[n] = abs(n) * rho(n)
-
-    # prepare sequence form 
-    if False :
-        # w/ circular shift?
-        alpha_seq = np.zeros(L)
-        for n in xrange(A,B+1) : alpha_seq[n] = alpha[n]
-        
-        #for n in xrange(B+1) : alpha_seq[n] = alpha[n]
-        #for n in xrange(A,0) :
-            #alpha_seq[ B+1 + n-A ] = alpha[n]
+        for road, segment in segment_dict.iteritems() :
+            match = roadbm.PREMATCH( segment )
+            MATCH.extend( match )
             
-    else :
-        alpha_seq = [ alpha[k] for k in xrange(A,B+1) ]
-        
-
-
-    
+            #surplus_dict[road] = SURPLUS( segment )
             
+            roadlen = ROAD.get_road_data( road, roadmap ).get( 'length', 1 )
+            measure = roadbm.MEASURE( segment, roadlen )
+            measure_dict[road] = measure
+            #objective_dict[road] = OBJECTIVE( measure )
+            #objective_dict[road] = objective
+        """ measure_dict now contains sequence of W_n """
     
+    
+        C_man = CONGESTION_OBJECTIVE_DATA( measure, rho, False )
+        C_fft = CONGESTION_OBJECTIVE_DATA( measure, rho, True )
         
+        C = CONGESTION_OBJECTIVE( measure, rho )
         
-    # long-hand, for verification
-    C_manual = {}
-    for z in xrange(-N,N+1) :
-        C_manual[z] = 0.
-        for k, w in measure.items() :
-            n = k + z
-            C_manual[z] += alpha[n] * w
-            
-    # efficient, FFT method
-    C_fft = sig.fftconvolve( W, alpha_seq )
-    
-    plt.figure()
-    plt.subplot(3,1,1)
-    plt.scatter( range(len(W)), W )
-    plt.subplot(3,1,2)
-    plt.scatter( range(len(alpha_seq)), alpha_seq )
-    plt.subplot(3,1,3)
-    plt.scatter( range(len(C_fft)), C_fft )
-    
-    
-    #C_right = C_fft[:B+1]
-    #C_left = C_fft[A:]
-    #C_fix = np.hstack( ( C_left, C_right ) )
-
-    nspace = range(-N,N+1)
-    C_fix = C_fft[b-A-N:b-A+N+1]
-    
-    compare = np.array([ C_manual[k] for k in xrange(-N,N+1) ])
-    error = C_fix - compare
-    
-    plt.figure()
-    plt.subplot(3,1,1)
-    plt.scatter( C_manual.keys(), C_manual.values() )
-    plt.subplot(3,1,2)
-    C = C_fix
-    plt.scatter( nspace, C_fix )
-    plt.subplot(3,1,3)
-    plt.scatter( nspace, error )
-    
-    
-    
-    
-    
-    
     
     
     
