67354d12533b468f997f380f3ee35c935477d06e
Kyle Treleaven <treleaven.kyle@gmail.com>
2014-08-06 21:22:33 -0400

    removing some redundancy in matchvis_post code

--- matchvis_post.py
+++ matchvis_post.py
@@ -48,26 +48,15 @@ if __name__ == '__main__' :
     interchanges = [ np.array(p) for p in interchanges ]
     N = len(interchanges)
     
+    
+    
+    
     """ construct roads from Delaunay adjacencies """
-    import scipy.spatial as spatial
-    tri = spatial.Delaunay( interchanges )
-    
-    import networkx as nx
-    graph = nx.Graph()
-    # find the edges in the triangulation
-    indices, seq = tri.vertex_neighbor_vertices
-    for i in xrange(N) :
-        for j in seq[ indices[i]:indices[i+1] ] :
-            graph.add_edge(i,j)
-    
-    """ build the roadmap data structure """
-    roadmap = nx.MultiDiGraph()
-    for ridx, (u,v) in enumerate( graph.edges() ) :
-        x, y = [ tri.points[k] for k in (u,v) ]
-        roadmap.add_edge(u,v, 'road %d' % ridx, length=np.linalg.norm(y-x) )
-        
-    """ and build positions dictionary """
-    pos = { k : point for k, point in enumerate( tri.points ) }
+    import setiptah.roadgeometry.generation as mapgen
+    roadmap = mapgen.DelaunayRoadMap( interchanges )
+            
+    """ ...and build positions dictionary """
+    pos = { k : point for k, point in enumerate( interchanges ) }
     
     
     """ now, obtain two sets of points """
